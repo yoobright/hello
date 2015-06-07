0174e23c6dcbcfd88334e8947fed168b6512adb3
yoobright <yxj3179@gmail.com>
2015-06-07 23:11:52 +0800

    hello

--- test_yield.py
+++ test_yield.py
@@ -7,9 +7,10 @@ def h():
     d = yield 12
     print 'We are together!'
 c = h()
-c1 = c.send(None) #相当于c.send(None)
+c1 = c.send(None)  # 相当于c.send(None)
 print 'c1:' + str(c1)
-c2 = c.send('Fighting!') #(yield 5)表达式被赋予了'Fighting!'输出的结果为：Wen Chuan Fighting!
+c2 = c.send('Fighting!')  # (yield 5)表达式被赋予了'Fighting!'输出的结果为：Wen Chuan
+# Fighting!
 print 'c2:' + str(c2)
 c3 = c.send(None)
 print 'c3:' + str(c3)
